bab08ed5eab4b66e685a245d59b9291ec14ed1b2
Laura <laura.perez@imotionanalytics.com>
2023-11-26 06:04:36 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,31 +17,27 @@ col1, col2 = st.columns(2)
 # Imagen 1
 with col1:
     image1_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_18.jpg?alt=media&token=278d1efd-9e57-4035-8656-8ffbdb28ee4e"
-    image1 = Image.open(BytesIO(requests.get(image1_url).content))
-    st.image(image1, caption="Imagen 1", use_column_width=True)
+    st.image(image1_url, caption="Imagen 1", use_column_width=True)
     if st.button("Pulsa para ejecutar1"):
         on_image_click("Imagen 1")
 
 # Imagen 2
 with col2:
     image2_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_10.jpg?alt=media&token=9f7ee55e-a6e2-4e58-b104-56a7e8da6de6"
-    image2 = Image.open(BytesIO(requests.get(image2_url).content))
-    st.image(image2, caption="Imagen 2", use_column_width=True)
+    st.image(image2_url, caption="Imagen 2", use_column_width=True)
     if st.button("Pulsa para ejecutar2"):
         on_image_click("Imagen 2")
 
 # Imagen 3
 with col1:
     image3_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_11.jpg?alt=media&token=d5dc1a73-b70e-4065-8db8-0c09b52ef8a0"
-    image3 = Image.open(BytesIO(requests.get(image3_url).content))
-    st.image(image3, caption="Imagen 3", use_column_width=True)
+    st.image(image3_url, caption="Imagen 3", use_column_width=True)
     if st.button("Pulsa para ejecutar3"):
         on_image_click("Imagen 3")
 
 # Imagen 4
 with col2:
     image4_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_12.jpg?alt=media&token=2dca730b-9ea2-4c5c-a3e0-83dec028e37a"
-    image4 = Image.open(BytesIO(requests.get(image4_url).content))
-    st.image(image4, caption="Imagen 4", use_column_width=True)
+    st.image(image4_url, caption="Imagen 4", use_column_width=True)
     if st.button("Pulsa para ejecutar4"):
         on_image_click("Imagen 4")
