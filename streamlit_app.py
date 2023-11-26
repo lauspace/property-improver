d11c687d9d9a87b08c80186324037d490ec06a43
Laura <laura.perez@imotionanalytics.com>
2023-11-26 06:27:47 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,57 @@
 import streamlit as st
-from PIL import Image
-from io import BytesIO
-import requests
 
-# Título principal y descripción
-st.title("Mi Streamlit App")
-st.write("Esta es una descripción para mi aplicación.")
+# Main title and description
+st.title("My Streamlit App")
+st.write("This is a description for my application.")
 
-# Función que se ejecutará al pulsar una imagen
+# Function to be executed when an image is clicked
 def on_image_click(image_name):
-    st.write(f"¡Has pulsado la imagen {image_name}!")
+    st.write(f"You clicked on image {image_name}!")
 
-# Grid de imágenes
+# Grid of images
 col1, col2 = st.columns(2)
 
-# Imagen 1
+# Add custom styles
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #1E1E1E; /* Dark background color */
+            color: #FFFFFF; /* White text color */
+        }
+        .stButton {
+            background-color: #FFA500; /* Orange button color */
+            color: #1E1E1E; /* Dark text color for buttons */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Image 1
 with col1:
     image1_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_18.jpg?alt=media&token=278d1efd-9e57-4035-8656-8ffbdb28ee4e"
     st.image(image1_url, use_column_width=True)
     if st.button("Property 1", use_container_width=True):
-        on_image_click("Imagen 1")
+        on_image_click("Image 1")
 
-# Imagen 2
+# Image 2
 with col2:
     image2_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_10.jpg?alt=media&token=9f7ee55e-a6e2-4e58-b104-56a7e8da6de6"
     st.image(image2_url, use_column_width=True)
     if st.button("Property 2", use_container_width=True):
-        on_image_click("Imagen 2")
+        on_image_click("Image 2")
 
-# Imagen 3
+# Image 3
 with col1:
     image3_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_11.jpg?alt=media&token=d5dc1a73-b70e-4065-8db8-0c09b52ef8a0"
     st.image(image3_url, use_column_width=True)
     if st.button("Property 3", use_container_width=True):
-        on_image_click("Imagen 3")
+        on_image_click("Image 3")
 
-# Imagen 4
+# Image 4
 with col2:
     image4_url = "https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_12.jpg?alt=media&token=2dca730b-9ea2-4c5c-a3e0-83dec028e37a"
     st.image(image4_url, use_column_width=True)
     if st.button("Property 4", use_container_width=True):
-        on_image_click("Imagen 4")
+        on_image_click("Image 4")
