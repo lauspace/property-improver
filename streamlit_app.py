b966e7e63009adcb5ec62d332a4a0019b36ca62f
Laura <laura.perez@imotionanalytics.com>
2023-11-26 05:52:16 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,24 +13,24 @@ col1, col2 = st.beta_columns(2)
 
 # Imagen 1
 with col1:
-    image1 = st.image("path/to/image1.jpg", caption="Imagen 1", use_column_width=True)
+    image1 = st.image("https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_18.jpg?alt=media&token=278d1efd-9e57-4035-8656-8ffbdb28ee4e", caption="Imagen 1", use_column_width=True)
     if image1.button("Pulsa para ejecutar"):
         on_image_click("Imagen 1")
 
 # Imagen 2
 with col2:
-    image2 = st.image("path/to/image2.jpg", caption="Imagen 2", use_column_width=True)
+    image2 = st.image("https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_18.jpg?alt=media&token=278d1efd-9e57-4035-8656-8ffbdb28ee4e", caption="Imagen 2", use_column_width=True)
     if image2.button("Pulsa para ejecutar"):
         on_image_click("Imagen 2")
 
 # Imagen 3
 with col1:
-    image3 = st.image("path/to/image3.jpg", caption="Imagen 3", use_column_width=True)
+    image3 = st.image("https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_18.jpg?alt=media&token=278d1efd-9e57-4035-8656-8ffbdb28ee4e", caption="Imagen 3", use_column_width=True)
     if image3.button("Pulsa para ejecutar"):
         on_image_click("Imagen 3")
 
 # Imagen 4
 with col2:
-    image4 = st.image("path/to/image4.jpg", caption="Imagen 4", use_column_width=True)
+    image4 = st.image("https://firebasestorage.googleapis.com/v0/b/property-improver-3.appspot.com/o/house3%2Fimg_18.jpg?alt=media&token=278d1efd-9e57-4035-8656-8ffbdb28ee4e", caption="Imagen 4", use_column_width=True)
     if image4.button("Pulsa para ejecutar"):
         on_image_click("Imagen 4")
